fc3fb001a144974906159dd869ccd8a0beb08fb0
Shaun Dashjian <shaundashjian@yahoo.com>
2019-06-04 12:38:13 -0700

    Created project

---
+++ src/main/java/com/shaundashjian/uvt/App.java
@@ -0,0 +1,9 @@
+package com.shaundashjian.uvt;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
---
+++ src/test/java/com/shaundashjian/uvt/AppTest.java
@@ -0,0 +1,14 @@
+package com.shaundashjian.uvt;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
