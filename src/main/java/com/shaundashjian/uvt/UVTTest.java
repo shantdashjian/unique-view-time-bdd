589ebb980c641a581887968b60495301ed466cf9
Shaun Dashjian <shaundashjian@yahoo.com>
2019-06-04 17:45:08 -0700

    Added interactive entry of viewed fragments from user in command line

--- src/main/java/com/shaundashjian/uvt/UVTTest.java
+++ src/main/java/com/shaundashjian/uvt/UVTTest.java
@@ -1,25 +1,62 @@
 package com.shaundashjian.uvt;
 
+import java.util.Scanner;
+
 public class UVTTest {
 
 	public static void main(String[] args) {
 		UvtCalculator uvtCalculator = new UvtCalculator();
 		
 		if (args.length == 0) {
-			System.out.println("No viewed fragments entered");
+			System.out.println("**********************************************");
+			System.out.println("*** Welcome to Unique View Time Calculator ***");
+			System.out.println("**********************************************");
+			System.out.println("Enter viewed fragments as pairs of times in milliseconds,");
+			System.out.println("separated by a space. Enter a pair per line.");
+			System.out.println();
+			System.out.println("Example:");
+			System.out.println("10 1000");
+			System.out.println("10000 20000");
+			System.out.println("10 100");
+			System.out.println("50 1000");
+			System.out.println();
+			System.out.println("***********************************************");
+			System.out.println();
+			Scanner scanner = new Scanner(System.in);
+			while (true) {
+				System.out.print("Enter fragment (or empty line to end): ");
+				String line = scanner.nextLine();
+				if (line.isEmpty()) {
+					break;
+				}
+				String[] times = line.split(" ");
+				try {
+					uvtCalculator.addFragment(
+							new Fragment(Integer.parseInt(times[0]), 
+									Integer.parseInt(times[1])));
+				} catch (Exception e) {
+					System.out.println("Incorrect number or format of input. Try again.");
+					continue;
+				}
+			}
+			System.out.println("Unique View Time: " + uvtCalculator.getUvt() + " milliseconds");
+			scanner.close();
 		} else {
 			if (args.length % 2 != 0) {
-				System.out.println("Incorrect format of fragments");
+				System.out.println("Incorrect number of fragments. Goodbye!!");
 			} else {
 				for (int i = 0; i < args.length; i += 2) {
-					uvtCalculator.addFragment(
-							new Fragment(Integer.parseInt(args[i]), 
+					try {
+						uvtCalculator.addFragment(
+								new Fragment(Integer.parseInt(args[i]), 
 										Integer.parseInt(args[i + 1])));
+					} catch (Exception e) {
+						System.out.println("Incorrect format of input. Gootbye!!");
+						System.exit(0);
+					}
 				}
 				System.out.println("Unique View Time: " + uvtCalculator.getUvt() + " milliseconds");
 			}
 		}
-
 	}
-
 }
